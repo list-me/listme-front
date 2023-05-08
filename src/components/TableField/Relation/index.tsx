2cc3863df63d1c8707a1341c239087780d5ec5ee
WandersonJunio <wanderson.junioantunes@outlook.com>
2023-05-08 09:20:24 -0300

    feat: tag products and add product button

--- src/components/TableField/Relation/index.tsx
+++ src/components/TableField/Relation/index.tsx
@@ -7,14 +7,31 @@ import { ReactComponent as AddIcon } from "../../../assets/add-gray-large.svg";
 
 export const Relation: React.FC<PropsRelation> = ({}) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [contentProducts, setContentProducts] = useState<any[]>([
+    "1",
+    "Product Product",
+    "Product2",
+    "Product3",
+    "Product4",
+  ]);
 
   const handleChangeVisible = () => setIsOpen(!isOpen);
 
   return (
     <Container>
       <Modal isOpen={isOpen} changeVisible={handleChangeVisible} />
-      <Tag> Wanderson </Tag>
-      <AddIcon onClick={handleChangeVisible} />
+      <div className="tagContent">
+        {contentProducts.length &&
+          contentProducts.map((content) => (
+            <Tag>
+              {" "}
+              <label>{content}</label>{" "}
+            </Tag>
+          ))}
+      </div>
+      <div className="imageContent">
+        <AddIcon onClick={handleChangeVisible} />
+      </div>
     </Container>
   );
 };
--- src/components/TableField/Relation/styles.ts
+++ src/components/TableField/Relation/styles.ts
@@ -4,21 +4,63 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
 
-  padding: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-  svg {
-    cursor: pointer;
+  /* padding: 8px; */
 
-    margin-left: 8px;
+  .tagContent {
+    display: flex;
+    flex-direction: row;
+
+    width: 100% !important;
+    overflow: hidden;
+
+    div:not(:first-child) {
+      margin-left: 0.5rem;
+    }
+
+    /* background-color: green; */
+  }
+
+  .imageContent {
+    width: 18px;
+
+    display: flex;
+    justify-content: center;
+
+    margin-right: -8px;
+    padding: 0 8px 0 6px;
+
+    svg {
+      cursor: pointer;
+    }
   }
 `;
 
 export const Tag = styled.div`
-  width: 80px !important;
+  min-width: 50px !important;
   height: 25px !important;
 
-  border-radius: 20px;
-  padding: 0 4px;
+  border-radius: 6px;
+  padding: 0px 6px;
 
   background: #dee2e6;
+
+  label {
+    overflow: hidden;
+
+    align-self: center;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
