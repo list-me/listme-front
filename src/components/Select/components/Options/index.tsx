12e3b6c41fac15663ac10f21938555855512f9a4
Victor Maya <victor.maya42@gmail.com>
2023-10-24 22:46:32 -0300

    fix: dropdown position

--- src/components/Select/components/Options/index.tsx
+++ src/components/Select/components/Options/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/destructuring-assignment */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ContainerOption, FixedOptions } from "./styles";
 
 const CustomMenuList = (props: any) => {
@@ -9,8 +9,25 @@ const CustomMenuList = (props: any) => {
   const regularOptions = childrenArray.slice(0, -2);
   const lastTwoOptions = childrenArray.slice(-2);
 
+  const [isMenuUp, setIsMenuUp] = useState(false);
+
+  useEffect(() => {
+    const handleMenuPosition = (): void => {
+      const selectMenu = document.querySelector(".containerOption");
+      const rect = selectMenu?.getBoundingClientRect();
+
+      if (rect && window.innerHeight - rect.bottom < 100) {
+        setIsMenuUp(true);
+      } else {
+        setIsMenuUp(false);
+      }
+    };
+
+    handleMenuPosition();
+  }, []);
+
   return (
-    <ContainerOption>
+    <ContainerOption isMenuUp={isMenuUp} className="containerOption">
       <div style={{ overflowY: "auto", maxHeight: "200px", padding: "8px" }}>
         {regularOptions}
       </div>
--- src/components/Select/components/Options/styles.ts
+++ src/components/Select/components/Options/styles.ts
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
 // eslint-disable-next-line import/prefer-default-export
-export const ContainerOption = styled.div`
+export const ContainerOption = styled.div<{
+  isMenuUp?: boolean;
+}>`
   background: ${({ theme }) => theme.colors.secondary};
   box-shadow: 0px 10px 40px 0px rgba(56, 24, 217, 0.07);
   border-radius: 8px;
-  position: absolute !important;
+  position: absolute;
   width: 100%;
   > div:active {
     background: ${({ theme }) => theme.colors.secondary} !important;
   }
+  bottom: ${(props) => props.isMenuUp && "78px"};
 `;
 export const FixedOptions = styled.div`
   margin: 0 8px;
