b5aa6edf90c11ca4feb20c74f013a08ddbe51f5c
Victor Maya <victor.maya42@gmail.com>
2023-10-26 15:12:31 -0300

    feat: type icon

--- src/components/Select/components/Option/index.tsx
+++ src/components/Select/components/Option/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/jsx-props-no-spreading */
 import { components } from "react-select";
-import { useState } from "react";
+import { ReactElement, useCallback, useMemo, useState } from "react";
 import { ReactComponent as ChevronDownIcon } from "../../../../assets/chevron-down-small.svg";
 import {
   ContainerNewColumn,
@@ -10,13 +10,60 @@ import {
   IconDropDown,
   ValueWidthDropDown,
 } from "./styles";
+import { ReactComponent as TextIcon } from "../../../../assets/icons/headers/text-icon.svg";
+import { ReactComponent as ParagraphIcon } from "../../../../assets/icons/headers/textarea-icon.svg";
+import { ReactComponent as CheckedIcon } from "../../../../assets/icons/headers/checked-icon.svg";
+import { ReactComponent as DropdownIcon } from "../../../../assets/icons/headers/dropdown-icon.svg";
+import { ReactComponent as FileIcon } from "../../../../assets/icons/headers/file-icon.svg";
+import { ReactComponent as RadioIcon } from "../../../../assets/icons/headers/radio-icon.svg";
+import { ReactComponent as RelationIcon } from "../../../../assets/icons/headers/relation-icon.svg";
+
+enum IconType {
+  Text = "text",
+  Paragraph = "paragraph",
+  Checked = "checked",
+  List = "list",
+  File = "file",
+  Radio = "radio",
+  Relation = "relation",
+}
 
 const CustomOption = (AdditionalComponent: any): JSX.Element => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
 
+  const ICON_HEADER = useMemo(
+    () => ({
+      [IconType.Text]: <TextIcon />,
+      [IconType.Paragraph]: <ParagraphIcon />,
+      [IconType.Checked]: <CheckedIcon />,
+      [IconType.List]: <DropdownIcon />,
+      [IconType.File]: <FileIcon />,
+      [IconType.Radio]: <RadioIcon />,
+      [IconType.Relation]: <RelationIcon />,
+    }),
+    [],
+  );
+
+  const getIconByType = useCallback(
+    (type: IconType): ReactElement => {
+      return ICON_HEADER[type];
+    },
+    [ICON_HEADER],
+  );
+
   // @ts-ignore
   return (props: any) => {
-    if (!props!.data.openDropdown) return <components.Option {...props} />;
+    const { label } = props?.data;
+    const { type } = props?.data;
+    if (!props!.data.openDropdown)
+      return (
+        <components.Option {...props}>
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            {getIconByType(type)}
+            <div>{label}</div>
+          </div>
+        </components.Option>
+      );
     return (
       <>
         <ContainerOption
