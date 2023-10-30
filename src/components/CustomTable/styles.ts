8750bcb87bd1acf0b8b159287ee1fe4936676174
Victor Maya <victor.maya42@gmail.com>
2023-10-30 15:47:56 -0300

    fix: points

--- src/components/CustomTable/styles.ts
+++ src/components/CustomTable/styles.ts
@@ -23,6 +23,7 @@ export const Container = styled.div`
   /* Arquivo CSS */
   .hot-table .corner {
     cursor: not-allowed;
+    pointer-events: none;
   }
 
   @media screen and (min-device-width: 1200px) and (max-device-width: 1600px) and (-webkit-min-device-pixel-ratio: 1) {
