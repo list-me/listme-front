1a21b62060a0876f711664f1f1b6ede1b33c3402
Victor Maya <victor.maya42@gmail.com>
2024-01-22 14:28:04 -0300

    remove consoles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -28,7 +28,6 @@ export const Input: React.FC<IInputProps> = ({
   disabledValidade,
 }) => {
   const [inputText, setInputText] = useState<string>(value || "");
-  console.log("🚀 ~ inputText:", inputText);
   const inputRef = useRef<InputRef | null>(null);
 
   const validateExactWord = (rule: any, word: any) => {
