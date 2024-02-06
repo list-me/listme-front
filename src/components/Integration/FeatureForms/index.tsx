e65d696b8bf3b77bdb387f30f2fa0594589236ba
Victor Maya <victor.maya42@gmail.com>
2024-02-06 14:50:37 -0300

    fix: bugs de caracteristicas

--- src/components/Integration/FeatureForms/index.tsx
+++ src/components/Integration/FeatureForms/index.tsx
@@ -106,7 +106,6 @@ function FeatureForms({
                       done={done}
                       headerSelectValue={headerSelectValues[index]}
                       setHeaderSelectValue={(e: any) => {
-                        console.log("veio");
                         changeListValue(
                           e,
                           index,
