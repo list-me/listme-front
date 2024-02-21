f4c917cbc58552f07f876233601d7affcc365da4
Victor Maya <victor.maya42@gmail.com>
2024-02-21 16:49:37 -0300

    remove consoles

--- src/context/products/index.tsx
+++ src/context/products/index.tsx
@@ -479,7 +479,6 @@ export const ProductContextProvider = ({
               type === "list" ||
               type === "boolean")
           ) {
-            console.log("veio");
             return [{ value: prevValue || "", destroy: true }];
           }
           if (!newValue && prevValue && type === "file") {
