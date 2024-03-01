7da61fcf3682374bee13ae5818970471ef7d8ae1
Victor Maya <victor.maya42@gmail.com>
2024-03-01 14:30:57 -0300

    ajustes nos headers dos steps

--- src/components/FromTo/index.tsx
+++ src/components/FromTo/index.tsx
@@ -18,9 +18,12 @@ const INITIAL_STEP = 0;
 function FromTo(): JSX.Element | null {
   const { currentStep, fromToIsOpened, setFromToIsOpened } = useFromToContext();
   const stepsArray = [
-    { title: "Importar arquivo de produtos", stepTitle: "Importar arquivo" },
-    { title: "Importar arquivo de produtos", stepTitle: "Config. de import." },
-    { title: "Importar arquivo de produtos", stepTitle: "Opç. de import." },
+    { title: "Configurações de importação", stepTitle: "Config. de import." },
+    {
+      title: "Selecione as opções de importação",
+      stepTitle: "Opç. de import.",
+    },
+    { title: "Configurações de integração", stepTitle: "Config. de integra." },
     { title: "Importar arquivo de produtos", stepTitle: "Vincular campos" },
   ];
   if (!fromToIsOpened) return null;
@@ -29,20 +32,20 @@ function FromTo(): JSX.Element | null {
       <BoxFromTo large={currentStep === 2}>
         <HeaderModal>
           <TitleModal>
-            {currentStep === 0
-              ? "Importar produtos"
+            {currentStep === 1
+              ? "Importar arquivo de produtos"
               : stepsArray[currentStep - 1].title}
           </TitleModal>
           <CloseButton onClick={() => setFromToIsOpened(false)}>
             <CloseIcon />
           </CloseButton>
         </HeaderModal>
-        {currentStep !== INITIAL_STEP && (
+        {currentStep !== INITIAL_STEP && currentStep !== 1 && (
           <StepsContainer>
             {stepsArray.map((item, index) => (
               <StepItem>
                 <StepNumber
-                  active={index + 1 <= currentStep}
+                  active={index + 2 <= currentStep}
                   className={
                     (index === 0 ? "firstStep " : "") +
                     (index + 1 === stepsArray.length ? "lastStep " : "")
@@ -50,7 +53,7 @@ function FromTo(): JSX.Element | null {
                 >
                   {index + 1}
                 </StepNumber>
-                <StepSubtitle active={index + 1 <= currentStep}>
+                <StepSubtitle active={index + 2 <= currentStep}>
                   {item.stepTitle}
                 </StepSubtitle>
               </StepItem>
