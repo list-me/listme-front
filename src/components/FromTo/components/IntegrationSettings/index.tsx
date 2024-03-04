4d9602b25b144e3554c963abfc9ab0e61816a57f
Victor Maya <victor.maya42@gmail.com>
2024-03-04 14:42:18 -0300

    multiselect

--- src/components/FromTo/components/IntegrationSettings/index.tsx
+++ src/components/FromTo/components/IntegrationSettings/index.tsx
@@ -1,36 +1,156 @@
 /* eslint-disable react/no-unescaped-entities */
-import React, { useState } from "react";
-import { Switch } from "antd";
-import { AlertSwitch, ContainerIntegrationSettings } from "./styles";
+import React, { useEffect, useRef, useState } from "react";
+import { Checkbox, Switch } from "antd";
+import {
+  AlertSwitch,
+  ButtonOpenOptions,
+  ContainerIntegrationSettings,
+  FixedItemIntegrationSettings,
+  ItemIntegrationSettings,
+  LabelMultiSelectIntegrationSettings,
+  ListContainerIntegrationSettings,
+  MultiSelectIntegrationSettings,
+  PseudoInputIntegrationSettings,
+} from "./styles";
 import { SwitchOption } from "../ImportOptions/styles";
 import { BoxButtons, NavigationButton } from "../../../NavigationButton/styles";
 import { ReactComponent as PlusIcon } from "../../../../assets/plus-fromto.svg";
 import { useFromToContext } from "../../../../context/FromToContext";
 import Select from "../../../Select";
+import IconNexaas from "../../../../assets/icons/MiniIconNexaas.png";
+import IconNuvemShop from "../../../../assets/icons/MiniIcon-NuvemShop.png";
+import IconShopify from "../../../../assets/icons/MiniIconShopify.png";
+import IconVTEX from "../../../../assets/icons/MiniIconVTEX.png";
+import { ReactComponent as ChevronIcon } from "../../../../assets/chevron-down-small.svg";
 
 function IntegrationSettings(): JSX.Element {
   const {
     setCurrentStep,
-    valuesIntegrationsConfig,
-    setValuesIntegrationsConfig,
+    // guardar valores aqui
+    // valuesIntegrationsConfig,
+    // setValuesIntegrationsConfig,
   } = useFromToContext();
   const [switchActive, setSwitchActive] = useState(false);
+
+  const [optionsOpened, setOptionsOpened] = useState(false);
+
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+
+  const icons: { [key: string]: any } = {
+    Nexaas: IconNexaas,
+    NuvemShop: IconNuvemShop,
+    Shopify: IconShopify,
+    VTEX: IconVTEX,
+  };
+
   const options = [
-    { value: "Nexaas", label: "Nexaas" },
-    { value: "NuvemShop", label: "NuvemShop" },
-    { value: "Shopify", label: "Shopify" },
-    { value: "VTEX", label: "VTEX" },
+    { value: "Nexaas", label: "Nexaas", icon: icons.Nexaas },
+    { value: "NuvemShop", label: "NuvemShop", icon: icons.NuvemShop },
+    { value: "Shopify", label: "Shopify", icon: icons.Shopify },
+    { value: "VTEX", label: "VTEX", icon: icons.VTEX },
   ];
+
+  const listToVerifyAllOptions = ["Nexaas", "NuvemShop", "Shopify", "VTEX"];
+
+  function verifyAllOptions(): boolean {
+    const selecteds = listToVerifyAllOptions.filter((item) => {
+      return selectedOptions.includes(item);
+    });
+    if (selecteds.length === listToVerifyAllOptions.length) {
+      return true;
+    }
+    return false;
+  }
+
+  const multiSelectRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (
+        multiSelectRef.current &&
+        // @ts-ignore
+        !multiSelectRef.current.contains(event.target)
+      ) {
+        setOptionsOpened(false);
+      }
+    }
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <ContainerIntegrationSettings>
-      <Select
-        select={valuesIntegrationsConfig[0]}
-        onChange={(value) => setValuesIntegrationsConfig([value])}
-        options={options}
-        placeHolder="Selecione"
-        labelText="Selecione as integrações para envio dos produtos"
-      />
+      <MultiSelectIntegrationSettings ref={multiSelectRef}>
+        <LabelMultiSelectIntegrationSettings>
+          Selecione as integrações para envio dos produtos
+          <PseudoInputIntegrationSettings>
+            <ButtonOpenOptions
+              onClick={() => setOptionsOpened((prev) => !prev)}
+              opened={optionsOpened}
+            >
+              {selectedOptions.length ? (
+                <div style={{ display: "flex", gap: "8px" }}>
+                  {selectedOptions.map((opt) => (
+                    <img src={icons[opt]} alt={opt} width={32} height={32} />
+                  ))}
+                </div>
+              ) : (
+                "Selecione"
+              )}
+              <ChevronIcon />
+            </ButtonOpenOptions>
+            {optionsOpened && (
+              <ListContainerIntegrationSettings>
+                <FixedItemIntegrationSettings>
+                  <Checkbox
+                    className="custom-checkbox"
+                    checked={verifyAllOptions()}
+                    onChange={() => {
+                      setSelectedOptions(
+                        verifyAllOptions() ? [] : listToVerifyAllOptions,
+                      );
+                    }}
+                  >
+                    Todas as integrações
+                  </Checkbox>
+                </FixedItemIntegrationSettings>
+                {options.map((opt) => (
+                  <ItemIntegrationSettings key={opt.label}>
+                    <Checkbox
+                      className="custom-checkbox"
+                      checked={selectedOptions.includes(opt.value)}
+                      onChange={() => {
+                        if (selectedOptions.includes(opt.value)) {
+                          const listWithoutItem = selectedOptions.filter(
+                            (item) => {
+                              return item !== opt.value;
+                            },
+                          );
+                          setSelectedOptions(listWithoutItem);
+                        } else {
+                          setSelectedOptions((prev) => [...prev, opt.value]);
+                        }
+                      }}
+                    >
+                      <img
+                        src={opt.icon}
+                        alt={opt.label}
+                        width={16}
+                        height={16}
+                      />
+                      {opt.label}
+                    </Checkbox>
+                  </ItemIntegrationSettings>
+                ))}
+              </ListContainerIntegrationSettings>
+            )}
+          </PseudoInputIntegrationSettings>
+        </LabelMultiSelectIntegrationSettings>
+      </MultiSelectIntegrationSettings>
       <SwitchOption>
         <p>Enviar todos os produtos da planilha para integração</p>
         <Switch
@@ -39,20 +159,16 @@ function IntegrationSettings(): JSX.Element {
           onChange={(e) => setSwitchActive(e)}
         />
       </SwitchOption>
-      <AlertSwitch active={switchActive}>
-        {switchActive ? (
-          <>
-            Todos os produtos de sua planilha serão enviados para as integrações
-            selecionadas
-          </>
-        ) : (
-          <>
-            <span>Para não enviar todos os produtos da sua planilha:</span>{" "}
-            Adicione a coluna 'have_import' à sua planilha CSV antes de importar
-            e utilize '1' ou 'true' para produtos a serem enviados, e '0' ou
-            'false' para os que não serão enviados.
-          </>
-        )}
+      <AlertSwitch>
+        <span>Para escolher quais produtos serão enviados:</span> Verifique se
+        sua planilha CSV contém a coluna: 'send_to_integrations' antes de
+        importar.
+        <br />
+        <br />
+        Utilize '1' ou 'true' para os produtos que serão salvos{" "}
+        <span>e ENVIADOS para as integrações;</span> E utilize '0' ou 'false'
+        para os produtos que serão salvos{" "}
+        <span>e NÃO ENVIADOS para as integrações.</span>
       </AlertSwitch>
       <BoxButtons>
         <NavigationButton
--- src/components/FromTo/components/IntegrationSettings/styles.ts
+++ src/components/FromTo/components/IntegrationSettings/styles.ts
@@ -2,19 +2,16 @@ import styled from "styled-components";
 
 export const ContainerIntegrationSettings = styled.div``;
 
-export const AlertSwitch = styled.p<{ active?: boolean }>`
+export const AlertSwitch = styled.p`
   color: ${({ theme }) => theme.colors.grayscale.primary};
   font-family: ${({ theme }) => theme.fonts.family.default};
   font-size: ${({ theme }) => theme.fonts.sizes.normal};
   font-weight: ${({ theme }) => theme.fonts.weights.regular};
   margin: 0;
-  background: ${(props) =>
-    props.active ? props.theme.colors.hover.background : "#fff9db"};
+  background: #fff9db;
   border-radius: 8px;
   padding: 16px;
-  border: 1px solid
-    ${(props) =>
-      props.active ? props.theme.colors.background.tertiary : "#fcc419"};
+  border: 1px solid #fcc419;
   margin-top: 8px;
   position: relative;
   span {
@@ -27,11 +24,113 @@ export const AlertSwitch = styled.p<{ active?: boolean }>`
     height: 0px;
     border-left: 8px solid transparent;
     border-right: 8px solid transparent;
-    border-bottom: 8px solid
-      ${(props) =>
-        props.active ? props.theme.colors.background.tertiary : "#fcc419"};
+    border-bottom: 8px solid #fcc419;
     position: absolute;
     right: 4px;
     top: -8px;
+  }
+`;
+
+export const MultiSelectIntegrationSettings = styled.div``;
+export const LabelMultiSelectIntegrationSettings = styled.label`
+  font-family: ${({ theme }) => theme.fonts.family.default};
+  font-weight: ${({ theme }) => theme.fonts.weights.bold};
+  font-size: ${({ theme }) => theme.fonts.sizes.normal};
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const PseudoInputIntegrationSettings = styled.div`
+  width: 100%;
+  height: 64px;
+  border-radius: 8px;
+  border: 1px solid #d1d6dc;
+  position: relative;
+  .custom-checkbox .ant-checkbox-inner {
+    background: ${({ theme }) => theme.colors.background.tertiary};
+    border-color: #bebafa;
+    border-width: 2px;
+  }
+  .custom-checkbox:hover .ant-checkbox-inner {
+    background: #bebafa;
+    border-color: #bebafa;
+  }
+
+  .ant-checkbox-checked .ant-checkbox-inner {
+    background: #3818d9 !important;
+    border-color: #3818d9;
+  }
+`;
+export const ListContainerIntegrationSettings = styled.div`
+  width: 101%;
+  left: -1px;
+  max-height: 216px;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.colors.secondary};
+  padding: 8px;
+  position: absolute;
+  top: calc(100% + 4px);
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  button {
+    padding: 0px 8px;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    width: 100%;
+    text-align: start;
+    height: 40px;
+    align-items: center;
+    label {
+      height: 40px;
+      font-family: ${({ theme }) => theme.fonts.family.default};
+      font-size: ${({ theme }) => theme.fonts.sizes.normal};
+      font-weight: ${({ theme }) => theme.fonts.weights.regular};
+      display: flex;
+      align-items: center;
+      width: 100%;
+    }
+    border-radius: 8px;
+    border: none;
+  }
+`;
+export const FixedItemIntegrationSettings = styled.button`
+  background: ${({ theme }) => theme.colors.background.tertiary};
+  label {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+export const ItemIntegrationSettings = styled.button`
+  background: ${({ theme }) => theme.colors.secondary};
+  label {
+    display: flex;
+    align-items: center;
+    span {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+  }
+`;
+export const ButtonOpenOptions = styled.button<{ opened?: boolean }>`
+  width: 100%;
+  height: 100%;
+  border: none;
+  background: none;
+  text-align: start;
+  padding: 20px;
+  font-family: ${(props) => props.theme.fonts.family.default};
+  font-size: ${(props) => props.theme.fonts.sizes.normal};
+  font-weight: ${(props) => props.theme.fonts.weights.regular};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  svg {
+    stroke: #000;
+    transform: ${(props) => (props.opened ? "rotate(180deg)" : "")};
+    transition: all 0.3s ease-in-out;
   }
 `;
