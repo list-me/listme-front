f43f9d554c0ed43a73875342ce846786d99d9fa5
Victor Maya <victor.maya42@gmail.com>
2024-07-17 11:59:45 -0300

    fix: console

--- src/components/FromTo/components/PublicList/PublicListList/components/PaginationTablePublicListList/index.tsx
+++ src/components/FromTo/components/PublicList/PublicListList/components/PaginationTablePublicListList/index.tsx
@@ -19,7 +19,6 @@ function PaginationTablePublicListListComponent({
   totalPages: number;
 }): JSX.Element {
   const buttons = Array(totalPages).fill("");
-  console.log("🚀 ~ totalPages:", totalPages);
 
   return (
     <PaginationTablePublicListList>
